684e25a0d59e76961ec121ba9afb4929f8956967
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-03-18 17:32:27 -0500

    flipping cards

--- js/app.js
+++ js/app.js
@@ -1,36 +1,77 @@
 console.log("hello")
 
-class Player1{
-	constructor(score, rounds){
-		this.score = 0
-		this.rounds = 0
+// class Player1{
+// 	constructor(score, rounds){
+// 		this.score = 0
+// 		this.rounds = 0
 
-	}
-}
+// 	}
+// }
 
 
 const game = {
 
-	score: 0,
-	rounds: 0,
+	// score: 0,
+	// rounds: 0,
+	//
+	firstCardFlipped: false,
+	firstCard: '',
 
 
-flipCard: function(event){
-	// console.log("i was clicked")
-	// console.log(this)
+	flipCard: function(event){
+		console.log(event.currentTarget); // <-- see what card was clicked in dataset -- 
+		if (event.querySelector('.backOfCard').hidden === false) {
+			event.querySelector('.backOfCard').hidden = true
+			event.querySelector('.frontOfCard').hidden = false
+		}
+		else {
+			event.querySelector('.backOfCard').hidden = false
+			event.querySelector('.frontOfCard').hidden = true
+		}
+		// console.log("i was clicked")
+
+		// if(!firstCardFlipped){
+		// 	firstCardFlipped = event.currentTarget
+		// 	console.log(firstCardFlipped)
+
+		// 	//fist card has been flipped
+
+		// 	// compoare
+		// }
+
+	},
+
+
+	compareCards: function(att){
+		// if att is same att cards = correct
+		// if (att === att){
+		// return true
+
+		// }
+
+	
+		console.log(att);
+
 
 	}
-
-
 }
 
-const cards = document.querySelectorAll('.game')
-for(let i = 0; i < cards.length; i++){
-cards[i].addEventListener('click', (event) =>{
-	const listen = event.target
-	game.flipCard(listen)
+// replace this with one listener -- use event.target to figure out what was clicked
+
+
+const cardDiv = document.querySelector('.memory')
+// for(let i = 0; i < cardDiv.length; i++){
+	cardDiv.addEventListener('click', (event) =>{
+		game.flipCard(event.currentTarget)
+		console.log(event.currentTarget)
+		//const target = event.target
+		//game.compareCards(cardDiv[i].getAttribute('data-card'))
+			
 	})
-}
+// }
+
+
+
 
 
 
